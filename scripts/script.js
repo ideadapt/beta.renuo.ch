3c8b4cae9a155aa1be8a6bb61dafc5b99ea3e1b7
Lukas Elmer <lukas.elmer@gmail.com>
2013-05-28 00:33:47 +0200

    Pages commit

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+(function() {
+  $(function() {
+    return $(document).ready(function() {
+      var map, mapOptions, marker, myLatlng;
+
+      if ($("#map_canvas").length >= 1) {
+        myLatlng = new google.maps.LatLng(47.421761, 8.499641);
+        mapOptions = {
+          zoom: 14,
+          center: myLatlng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          draggable: false,
+          zoomControl: false,
+          scrollwheel: false,
+          disableDoubleClickZoom: false,
+          keyboardShortcuts: false,
+          overviewMapControl: false,
+          panControl: false,
+          rotateControl: false,
+          scaleControl: false,
+          streetViewControl: false,
+          mapTypeControl: false
+        };
+        map = new google.maps.Map($('#map_canvas')[0], mapOptions);
+        myLatlng = new google.maps.LatLng(47.4194, 8.499308);
+        marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Renuo GmbH',
+          clickable: true
+        });
+        return google.maps.event.addListener(marker, 'click', function() {
+          return window.open('https://maps.google.ch/maps?q=Renuo+GmbH,+Hungerbergstrasse+26,+Z%C3%BCrich&hl=de&ie=UTF8&hq=Renuo+GmbH,&hnear=Hungerbergstrasse+26,+Kreis+11,+8046+Z%C3%BCrich&t=m&z=16&iwloc=A');
+        });
+      }
+    });
+  });
+
+}).call(this);
